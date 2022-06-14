5c09583653e4d258a9e1b114d240ec54e55319b4
Philip <philipcho101@hotmail.com>
2022-06-13 21:02:15 -0700

    Add active class to show current tab

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ const headerElement = () => {
     for (let i = 0; i < headerItems.length; i++) {
         const li = document.createElement('li');
         li.textContent = headerItems[i];
+        li.classList.add(headerItems[i].toLowerCase());
+        if (i == 0) {
+            li.classList.add('active');
+        }
+        li.addEventListener('click', switchTabs);
         ul.appendChild(li);
     }
     return header;
@@ -19,31 +24,41 @@ const headerElement = () => {
 const footerElement = () => {
     const footer = document.createElement('footer');
     const p = document.createElement('p');
-    p.textContent = 'Copyright © 2022 Restaurant Name | pcho101';
+    const a = document.createElement('a');
+    a.href = 'https://github.com/pcho101/top-restaurant-page';
+    p.textContent = 'Copyright © 2022 | ';
+    a.textContent = 'pcho101';
+    p.appendChild(a)
     footer.appendChild(p);
     return footer;
 }
+
+const switchTabs = (e) => {
+    const content = document.getElementById('content');
+    if (e.target.classList.contains('active')) {
+        return;
+    }
+    setActive(e.target);
+    if (e.target.textContent == headerItems[0]) {
+        content.replaceChild(home(), content.children[1]);
+    }
+    else if (e.target.textContent == headerItems[1]) {
+        content.replaceChild(menu(), content.children[1]);
+    }
+    else if (e.target.textContent == headerItems[2]) {
+        content.replaceChild(contact(), content.children[1]);
+    }
+}
+
+const setActive = (node) => {
+    const active = document.querySelector('.active');
+    active.classList.remove('active');
+    node.classList.add('active');
+};
 
 const displayPage = (() => {
     const content = document.getElementById('content');
     content.appendChild(headerElement());
     content.appendChild(home());
     content.appendChild(footerElement());
-})();
-
-const switchTabs = (() => {
-    document.querySelectorAll('li').forEach(node => {
-        node.addEventListener('click', () => {
-            const content = document.getElementById('content');
-            if (node.textContent == headerItems[0]) {
-                content.replaceChild(home(), content.children[1]);
-            }
-            else if (node.textContent == headerItems[1]) {
-                content.replaceChild(menu(), content.children[1]);
-            }
-            else if (node.textContent == headerItems[2]) {
-                content.replaceChild(contact(), content.children[1]);
-            }
-        });
-    });
 })();
